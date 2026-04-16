64decb98d8738104d83377b74ce8b47a98f656d4
Ricardo Lara <riclara@gmail.com>
2026-04-16 17:55:50 -0500

    chore(main): release financialclaw 1.2.0

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 export const PACKAGE_NAME = "financialclaw";
-export const PACKAGE_VERSION = "1.1.0"; // x-release-please-version
+export const PACKAGE_VERSION = "1.2.0"; // x-release-please-version
